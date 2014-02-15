82890f1d76c1bcbdce1729b32d47ce6be275877d
Daredevils <daredevils2512@gmail.com>
2014-02-15 15:54:03 -0600

    Adjusted controls

---
+++ src/Commands/StationPickUp.h
@@ -0,0 +1,13 @@
+#ifndef StationPickUp_H
+#define StationPickUp_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class StationPickUp: public CommandGroup
+{
+public:
+	StationPickUp();
+};
+
+#endif
