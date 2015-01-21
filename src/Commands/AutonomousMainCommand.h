bcc5f88225fb891c9e9fdb33475442a1cac03dc6
Rcstarttofinish <rcstarttofinish@gmail.com>
2015-01-20 18:01:02 -0600

    Added autonomous command groups.
    Conflicts:
    	RecycleRushBot.yml
    	src/OI.cpp

---
+++ src/Commands/AutonomousMainCommand.h
@@ -0,0 +1,14 @@
+#ifndef AutonomousMainCommand_H
+#define AutonomousMainCommand_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+#include "../Robot.h"
+
+class AutonomousMainCommand: public CommandGroup
+{
+public:
+	AutonomousMainCommand(int containers);
+};
+
+#endif
