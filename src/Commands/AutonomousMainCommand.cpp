09c08b28db513b9181579833d56e5a6d3353b834
Rcstarttofinish <rcstarttofinish@gmail.com>
2015-01-20 18:01:02 -0600

    Added autonomous command groups.

---
+++ src/Commands/AutonomousMainCommand.cpp
@@ -0,0 +1,47 @@
+#include "AutonomousMainCommand.h"
+#include "RunIntake.h"
+#include "AutonDrive.h"
+#include "RunWinch.h"
+
+AutonomousMainCommand::AutonomousMainCommand(int containers)
+{
+	// Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.
+
+	AddSequential(new RunIntake());
+
+
+	if (containers > 0) AddParallel(new RunWinch(1));
+	AddSequential(new AutonDrive(0.25, 0, 1));
+	//	lift
+
+	if (containers > 1) AddParallel(new RunWinch(2));
+	AddSequential(new AutonDrive(0.25, 0, 1));
+	// ratchet
+	//	lift
+
+
+	if (containers > 2) AddParallel(new RunWinch(3));
+	AddSequential(new AutonDrive(0.25, 0, 1));
+	// ratchet
+	//	lift
+
+	AddSequential(new AutonDrive(0.25, 90, 1));
+	AddSequential(new AutonDrive(0.25, 0, 1));
+
+//  put down
+}
