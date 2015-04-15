d1b1f43c7f82b1563f0fd20ed75e22ae1105b6ac
Craig <hsiaocraig@gmail.com>
2015-04-15 17:14:32 -0500

    Added camera code.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -71,6 +71,18 @@ void Robot::RobotInit() {
 //	CameraServer::GetInstance()->StartAutomaticCapture("cam0");
 
 //	CameraServer::GetInstance()->StartAutomaticCapture("cam0");
+
+	// create an image
+	frame = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
+	//the camera name (ex "cam0") can be found through the roborio web interface
+	imaqError = IMAQdxOpenCamera("cam0", IMAQdxCameraControlModeController, &session);
+	if(imaqError != IMAQdxErrorSuccess) {
+		DriverStation::ReportError("IMAQdxOpenCamera error: " + std::to_string((long)imaqError) + "\n");
+	}
+	imaqError = IMAQdxConfigureGrab(session);
+	if(imaqError != IMAQdxErrorSuccess) {
+		DriverStation::ReportError("IMAQdxConfigureGrab error: " + std::to_string((long)imaqError) + "\n");
+	}
   }
 
 /**
@@ -127,9 +139,22 @@ void Robot::TeleopInit() {
 		autonomousCommand->Cancel();
 	clawPID->Disable();
 	intakeSystem->ActuateIntake(false);
+
+	// acquire images
+	IMAQdxStartAcquisition(session);
 }
 
 void Robot::TeleopPeriodic() {
+	while(IsOperatorControl() && IsEnabled()) {
+		IMAQdxGrab(session, frame, true, NULL);
+		if(imaqError != IMAQdxErrorSuccess) {
+			DriverStation::ReportError("IMAQdxGrab error: " + std::to_string((long)imaqError) + "\n");
+		} else {
+			CameraServer::GetInstance()->SetImage(frame);
+		}
+		Wait(0.005);				// wait for a motor update time
+	}
+
 	Scheduler::GetInstance()->Run();
 
 //	SmartDashboard::PutData("Camera", CameraServer::GetInstance());
