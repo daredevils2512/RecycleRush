b8d441dd9ebf48c88bdc9c3dce1aa7f0902a04f8
Daredevils <daredevils2512@gmail.com>
2015-04-24 21:01:10 -0500

    Added center container grab autonomous.

---
+++ src/Commands/CenterCanGrab.h
@@ -0,0 +1,13 @@
+#ifndef CenterCanGrab_H
+#define CenterCanGrab_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class CenterCanGrab: public CommandGroup
+{
+public:
+	CenterCanGrab();
+};
+
+#endif
